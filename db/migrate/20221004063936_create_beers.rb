90bcc5d852d73e8ab28c4bb534f1bfb7fb7a49d1
javaman-22 <98050472+javaman-22@users.noreply.github.com>
2022-11-20 15:08:57 +0100

    week 5 done

--- db/migrate/20221004063936_create_beers.rb
+++ db/migrate/20221004063936_create_beers.rb
@@ -2,7 +2,7 @@ class CreateBeers < ActiveRecord::Migration[7.0]
   def change
     create_table :beers do |t|
       t.string :name
-      t.string :style
+      t.string :old_style
       t.integer :brewery_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_05_095959) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_20_134132) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_095959) do
     t.integer "brewery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "style_id"
   end
 
   create_table "breweries", force: :cascade do |t|
@@ -38,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_095959) do
     t.integer "beer_club_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "ratings", force: :cascade do |t|
@@ -46,6 +48,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_095959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
